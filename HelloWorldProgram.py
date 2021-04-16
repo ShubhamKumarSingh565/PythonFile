4d758cbf4c132dd9e7c42b51d33ba88bc8c75553
Shubhamsingh565 <63440492+Shubhamsingh565@users.noreply.github.com>
2021-04-16 21:19:02 +0530

    Basic syntax of C, C++, Java, and Python

---
+++ HelloWorldProgram.py
@@ -0,0 +1,34 @@
+# Compare with C, C++, and Java
+
+# C Language
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    printf("Hello world");
+    return 0;
+}
+
+
+# C++ Language
+
+#include<iostream.h>
+using namespace std;
+int main(){
+    cout<<"Hello world";
+    return 0;
+}
+
+
+# Java Language
+
+public class HelloWorld{
+     public static void main(String[] args){
+          System.out.println("Hello world");
+     }
+}
+
+
+#Python Language
+
+print("Hello World")
